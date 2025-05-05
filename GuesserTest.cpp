5f54c99e0831f5065fe9850caa59186c9a4656e9
cesar <cesargfranco270@gmail.com>
2025-05-04 22:37:46 -0700

    Test cases added

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -14,9 +14,39 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+TEST(GuesserTest, Capitalize_test)
+{
+  Guesser object("Celebrate");
+  ASSERT_FALSE(object.match("CeleBrate")); // Try 1
+  ASSERT_EQ(object.remaining(), 1);
+
+  ASSERT_FALSE(object.match("Celebratexxx")); // Try 2
+  ASSERT_EQ(object.remaining(), 3);
+
+  ASSERT_FALSE(object.match("Celebrate")); // Locked
+  ASSERT_EQ(object.remaining(), 0);
+}
+TEST(GuesserTest, Happy_test)
+{
+  Guesser object("Happy");
+  ASSERT_TRUE(object.match("Happy")); // Try 2
+  ASSERT_EQ(object.remaining(), 0);
+}
+TEST(GuesserTest, ExtraChar_test)
+{
+  Guesser object("Password");
+  ASSERT_FALSE(object.match("PasswordMotel")); // Try 1
+  ASSERT_EQ(object.remaining(), 5);
+
+  ASSERT_FALSE(object.match("PasswordAppleEWQ")); // Try 2
+  ASSERT_EQ(object.remaining(), 8);
+}
+TEST(GuesserTest, LessChar_test)
 {
   Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+  ASSERT_FALSE(object.match("sec")); // Try 1
+  ASSERT_EQ(object.remaining(), 4);
+
+  ASSERT_FALSE(object.match("Secr")); // Try 2
+  ASSERT_EQ(object.remaining(), 2);
 }
